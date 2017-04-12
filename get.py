65a2244bb3406bcce5fbe43e25b67b60ef6fdc78
MentalFS <git@mentalfs.de>
2017-04-12 09:47:25 +0200

    Adding get.py with last modification time

---
+++ get.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import gw2api, json, argparse
+
+argparser=argparse.ArgumentParser(description='Get raw json from GW2 API v2')
+argparser.add_argument(dest='url', metavar='URL', help='URL')
+argparser.add_argument('-p', '--profile', dest='profile', default='default', help='Profile in INI file')
+args = argparser.parse_args()
+
+config = gw2api.init(args.profile)
+
+
+print json.dumps(gw2api.get_single(args.url), indent=4);
